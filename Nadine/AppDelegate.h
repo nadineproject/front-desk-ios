e519009c49eb1c0b11a211638fc38c3d9510059a
Will Clarke <willclarke@Williams-MacBook-Pro-2.local>
2014-10-03 14:43:28 -0700

    Initial Commit

---
+++ Nadine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nadine
+//
+//  Created by Will Clarke on 10/3/14.
+//  Copyright (c) 2014 Office Nomads. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nadine/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Nadine
+//
+//  Created by Will Clarke on 10/3/14.
+//  Copyright (c) 2014 Office Nomads. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
