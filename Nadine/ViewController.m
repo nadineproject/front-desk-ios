94636a43d21b1a58f9d7d9ee4c85ce81e6a814fa
Will Clarke <willclarke@Williams-MacBook-Pro-2.local>
2014-10-03 16:38:15 -0700

    First commit yay

--- Nadine/ViewController.m
+++ Nadine/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView *mainWebView;
 
 @end
 
@@ -17,11 +18,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [UIApplication sharedApplication].idleTimerDisabled = YES;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    NSURL *nadineURL = [NSURL URLWithString: @"http://172.16.4.29:8000/tablet/"];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:nadineURL];
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    [NSURLConnection sendAsynchronousRequest:request queue:queue completionHandler:^(NSURLResponse *response, NSData *data, NSError *error)
+     {
+         if ([data length] > 0 && error == nil) [self.mainWebView loadRequest:request];
+         else if (error != nil) NSLog(@"Error: %@", error);
+     }];
+}
+
+-(UIStatusBarStyle)preferredStatusBarStyle{
+    return UIStatusBarStyleLightContent;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)refreshButtonTapped:(id)sender {
+    [self.mainWebView reload];
+    NSLog(@"Refresh Button Tapped");
+}
+
+- (IBAction)homeButtonTapped:(id)sender {
+    NSLog(@"Home Button Tapped");
+}
+
 
 @end
