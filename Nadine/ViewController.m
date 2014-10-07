a2420080bfd61b0ca534a297b8a6ab71daf7ed51
Will Clarke <willclarke@Williams-MacBook-Pro-2.local>
2014-10-07 11:11:22 -0700

    Added top buttons and bottom bar

--- Nadine/ViewController.m
+++ Nadine/ViewController.m
@@ -11,6 +11,8 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIWebView *mainWebView;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *topButtons;
+
 
 @end
 
@@ -54,5 +56,37 @@
     NSLog(@"Home Button Tapped");
 }
 
+- (void)clearButtonsAndSetActive:(UIButton *)activeButton {
+    for (UIButton *currentButton in self.topButtons) {
+        if (currentButton == activeButton) {
+            currentButton.backgroundColor = [UIColor colorWithRed:51/255.f green:136/255.f blue:204/255.f alpha:1];
+            [currentButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        } else {
+            currentButton.backgroundColor = [UIColor colorWithWhite:246/255.f alpha:1];
+            [currentButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        }
+    }
+}
+
+- (IBAction)membersButtonTapped:(id)sender {
+    [self clearButtonsAndSetActive:(UIButton *)sender];
+    NSURL *nadineURL = [NSURL URLWithString: [NSString stringWithFormat:@"%@tablet/members/", NADINE_BASE_URL]];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:nadineURL];
+    [self.mainWebView loadRequest:request];
+}
+
+- (IBAction)hereTodayButtonTapped:(id)sender {
+    [self clearButtonsAndSetActive:(UIButton *)sender];
+    NSURL *nadineURL = [NSURL URLWithString: [NSString stringWithFormat:@"%@tablet/here_today/", NADINE_BASE_URL]];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:nadineURL];
+    [self.mainWebView loadRequest:request];
+}
+
+- (IBAction)visitorsButtonTapped:(id)sender {
+    [self clearButtonsAndSetActive:(UIButton *)sender];
+    NSURL *nadineURL = [NSURL URLWithString: [NSString stringWithFormat:@"%@tablet/visitors/", NADINE_BASE_URL]];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:nadineURL];
+    [self.mainWebView loadRequest:request];
+}
 
 @end
